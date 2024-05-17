7d1f16864af7e7e189bd52e69d7a7f7abcd32e1f
EdisonSheppard <116647439+EdisonSheppard@users.noreply.github.com>
2024-05-17 21:15:09 +1000

    123js

---
+++ jerverscript.js
@@ -0,0 +1,244 @@
+mycanvas = document.getElementById("myCanvas");
+myContext = mycanvas.getContext("2d");
+mycanvas.width = window.screen.width;
+mycanvas.height = window.screen.height;
+
+function drawSomeSquares()
+{
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+	
+	myContext.fillRect(Math.random()*window.screen.width, Math.random()*window.screen.height, Math.random()*30, Math.random()*30);
+	myContext.fillStyle = "rgb(" + (Math.random()*255) + " " + (Math.random()*255) + " " + (Math.random()*255) + ")";
+}
+
+function drawmanysquares()
+{
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+	drawSomeSquares();
+}
+
+function drawlotsofsquares()
+{
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+	drawmanysquares();
+}
+
+function drawtonsofsquares()
+{
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+	drawlotsofsquares();
+}
+
+function aaa()
+{
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+	drawtonsofsquares();
+}
+function aab()
+{
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+	aaa();
+}
+
+function aac()
+{ 
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+	aab();
+}
+setInterval(drawlotsofsquares, 1);
